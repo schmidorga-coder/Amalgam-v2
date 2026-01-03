dec1f929c81efc79d6483d752ba2c8b4ec1750b0
Heinrich <schmidorga@gmail.com>
2026-01-03 03:31:13 +0100

    Update AutoJoin.h

--- Amalgam/src/Features/AutoJoin/AutoJoin.h
+++ Amalgam/src/Features/AutoJoin/AutoJoin.h
@@ -1,13 +1,26 @@
 #pragma once
 #include "../../SDK/SDK.h"
+#include <array>
 
 class CAutoJoin
 {
 private:
-	const std::array<std::string, 9u> m_aClassNames{ "scout", "sniper", "soldier", "demoman", "medic", "heavyweapons", "pyro", "spy", "engineer" };
+    // Map TF2 Class IDs (1-9) to console command strings.
+    // 1:Scout, 2:Sniper, 3:Soldier, 4:Demoman, 5:Medic, 6:Heavy, 7:Pyro, 8:Spy, 9:Engineer
+    static constexpr std::array<const char*, 9> m_aClassNames = {
+        "scout",
+        "sniper",
+        "soldier",
+        "demoman",
+        "medic",
+        "heavyweapons",
+        "pyro",
+        "spy",
+        "engineer"
+    };
 
 public:
-	void Run(CTFPlayer* pLocal);
+    void Run(CTFPlayer* pLocal);
 };
 
 ADD_FEATURE(CAutoJoin, AutoJoin)
